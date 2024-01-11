7523287cfb6e784c5abff3973f085118c3e8e250
jkenttech <contact@jkent.tech>
2024-01-12 00:32:10 +1100

    fixed some formatting:

--- 1. Two Sum/TwoSum.cpp
+++ 1. Two Sum/TwoSum.cpp
@@ -7,14 +7,14 @@ class Solution
 {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-       std::vector<int> result = {};
-       for(int i=0; i < nums.size()-1; i++){
-           for(int j=1; j < nums.size(); j++){
-               if(i != j && nums[i] + nums[j] == target){
-                   result.push_back(i);
-                   result.push_back(j);
-									 std::cout << "[" << i << "][" << j << "], " << nums[i] << "+" << nums[j] << "=" << target << std::endl;
-                   return result;
+        std::vector<int> result = {};
+        for(int i=0; i < nums.size()-1; i++){
+            for(int j=1; j < nums.size(); j++){
+                if(i != j && nums[i] + nums[j] == target){
+                    result.push_back(i);
+                    result.push_back(j);
+				    std::cout << "[" << i << "][" << j << "], " << nums[i] << "+" << nums[j] << "=" << target << std::endl;
+                    return result;
                }
            }
        } 
@@ -24,19 +24,18 @@ public:
 
 int main()
 {
-		std::map<int, std::vector<int>> tcmap;
+	std::map<int, std::vector<int>> tcmap;
 
-		tcmap[9] = {2,7,11,15};
-		tcmap[6] = {3,2,4};
-		tcmap[4] = {1, 7, 9, 5, 2, 3};
+	tcmap[9] = {2,7,11,15};
+	tcmap[6] = {3,2,4};
+	tcmap[4] = {1, 7, 9, 5, 2, 3};
 
-		Solution sl = Solution();
-		std::map<int, std::vector<int>>::iterator itr = tcmap.begin();
-		while (itr != tcmap.end())
-		{
-				sl.twoSum(itr->second, itr->first);
-				itr++;
-		}
-
-		return 0;
+	Solution sl = Solution();
+	std::map<int, std::vector<int>>::iterator itr = tcmap.begin();
+	while (itr != tcmap.end())
+	{
+		sl.twoSum(itr->second, itr->first);
+		itr++;
+	}
+	return 0;
 }
