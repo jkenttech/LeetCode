c0c0be62b6f65b916b422ca1efdecfc90c4c7de0
jkenttech <contact@jkent.tech>
2024-01-13 08:09:57 +1100

    Added startercode and readme for problem 3:

---
+++ 3. Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <map>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        
+    }
+};
+
+int main()
+{
+    Solution sl = Solution();
+
+    std::map<std::vector<int>, std::vector<int>> tcmap;
+
+    std::map<std::vector<int>, std::vector<int>>::iterator itr = tcmap.begin();
+    while (itr != tcmap.end())
+    {
+        itr++;
+    }
+
+    return 0;
+}
+
