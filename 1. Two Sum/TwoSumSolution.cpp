f7ec2af1dab195f9b6ce38b416dc185a7d9990f8
JKentTech <contact@jkent.tech>
2024-12-05 01:08:10 +1100

    Update TwoSumSolution.cpp

--- 1. Two Sum/TwoSumSolution.cpp
+++ 1. Two Sum/TwoSumSolution.cpp
@@ -4,6 +4,7 @@
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //You can return the answer in any order.
 
+class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
         
